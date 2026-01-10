528de9bade7c37b37fb80121bacc6e2c886ca7fc
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-09 19:54:17 -0500

    adding loop over list of rules example

--- examples/example_to_pandas_mask.py
+++ examples/example_to_pandas_mask.py
@@ -2,3 +2,10 @@ from src.stock_screener.data_models.filter import FilterRule as rule
 
 mask = rule.to_mask(df)
 df = df[mask]
+
+
+# Filter over multiple rules
+for rule in rules:
+    df = df[
+        rule.to_pandas_mask(df)
+    ]
