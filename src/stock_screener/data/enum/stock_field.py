26408923fae7fa81081b134eec61947ca5247285
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-12 14:31:42 -0500

    adding StockField enum

--- src/stock_screener/data/enum/stock_field.py
+++ src/stock_screener/data/enum/stock_field.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class StockField(str, Enum):
-    dividen_yield = "dividen_yield"
+    dividend_yield = "dividend_yield"
     market_cap = "market_cap"
     avg_daily_volume = "avg_daily_volume"
     pe_ratio = "pe_ratio"
