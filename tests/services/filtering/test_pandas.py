9161795320fab1c0cbd03e088d974033c294c0a4
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-13 16:08:05 -0500

    map out unit tests for panda's operator mask

---
+++ tests/services/filtering/test_pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from stock_screener.data.models.filter import FilterRule
+from stock_screener.data.services.filtering.pandas import rule_to_mask
+
+
+def test_apply_filter_greater_than():
+    pass
+
+def test_apply_filter_less_than():
+    pass
+
+def test_apply_filter_greater_than_or_equal_to():
+    pass
+
+def test_apply_filter_less_than_or_equal_to():
+    pass
+
+def test_apply_filter_equal_to():
+    pass
