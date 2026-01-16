ca8fa7262fdeae635746d9836ff6fee5e49de17d
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-16 14:38:00 -0500

    creating MVP columns for covered call strategy

--- src/stock_screener/data/enums/stock_field.py
+++ src/stock_screener/data/enums/stock_field.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
 class StockField(str, Enum):
+    ticker = 'ticker'
+    price = 'price'
+    avg_daily_volume = 'avg_daily_volume'
     dividend_yield = "dividend_yield"
     market_cap = "market_cap"
-    avg_daily_volume = "avg_daily_volume"
     pe_ratio = "pe_ratio"
     pb_ratio = "pb_ratio"
-    price ="price"
+    sector = "sector"
